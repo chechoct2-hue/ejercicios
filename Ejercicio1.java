81d9ec9125801c2f331c07788843b24d5a0249a2
chechoct2-hue <chechoct2@gmail.com>
2025-10-01 16:29:43 -0500

    primer cambio
    este es mi primer cambio

---
+++ Ejercicio1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        String[] nombres = new String[5];
+
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.print("Ingresa el nombre #" + (i + 1) + ": ");
+            nombres[i] = sc.nextLine();
+        }
+        
+        System.out.println("Nombres con mas de 3 letras" ) ;
+        for ( String nombre : nombres){
+            if ( nombre.length() >=3){
+                System.out.println(nombre);
+                
+            }
+            // hola soy un comentario
+
+            // este es un segundo comentario
+          
+            
+        }
+    }
+}
